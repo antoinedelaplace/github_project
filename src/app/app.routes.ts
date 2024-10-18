0eedf295aae437e41fc0e5c52cdfff9b3c305252
antoinedelaplace <antoine.delaplace@gmail.com>
2024-10-18 17:48:29 +0200

    Remove unused features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,25 +9,6 @@ export const routes: Routes = [
 			)
 	},
 	{
-		path: 'users',
-		loadComponent: () =>
-			import('./features/users/users.component').then(m => m.UsersComponent)
-	},
-	{
-		path: 'files',
-		loadComponent: () =>
-			import('./features/google-drive-files/google-drive-files.component').then(
-				m => m.GoogleDriveFilesComponent
-			)
-	},
-	{
-		path: 'google-redirect',
-		loadComponent: () =>
-			import(
-				'./features/google-drive-files/google-auth-redirect.component'
-			).then(m => m.GoogleAuthRedirectComponent)
-	},
-	{
 		path: 'github-repositories',
 		loadComponent: () =>
 			import(
--- src/app/features/users/store/user.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-
-import { User } from './user.model';
-
-export const addUser = createAction(
-	'[User List] Add User',
-	props<{ user: User }>()
-);
